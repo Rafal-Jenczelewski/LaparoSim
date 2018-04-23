ad1c1136e3bd4ca39ad48c011262896e589b25fd
RafalPC <r.jenczelewski@gmail.com>
2018-04-23 11:25:20 +0200

    Added tester projects

---
+++ LaparoTester/LaparoTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaparoCommunicator;
+
+namespace LaparoTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var comm = LaparoCommunicator.CommunicatorFactory.GetMock(@"C:/data/"))
+            {
+                Console.WriteLine(comm.GetDataInQuaternion());
+                Console.WriteLine(comm.GetDataInEuler());
+                Console.WriteLine(comm.GetDataInCartesian());              
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ LaparoTester2/LaparoTester2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaparoCommunicator;
+
+namespace LaparoTester2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var comm = CommunicatorFactory.GetCommunicator())
+            {
+                for (long i = Int64.MinValue; i < Int64.MaxValue; i++)
+                        ;// Console.WriteLine("iter" + i + j);
+            }
+            Console.ReadKey();
+        }
+    }
+}
